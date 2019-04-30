00e07360f5e549ca5091c51d0fe00503e4b2e172
nicolas_azzopardi@yahoo.fr <nicolas_azzopardi@yahoo.fr>
2019-04-30 17:21:27 +0200

    ior.R

---
+++ R/ior.R
@@ -0,0 +1,11 @@
+#' Install or require a list of packages.
+#' @param pack List of packages names to require or if not installed, to install and require.
+#' @keywords package
+#' @examples # ior("ggplot2")
+
+ior <- function(pack){
+  create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
+  if (length(create.pkg))
+    install.packages(create.pkg, dependencies = TRUE)
+  sapply(pack, require, character.only = TRUE)
+}
