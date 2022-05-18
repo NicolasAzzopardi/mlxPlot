2e2f8d8287e1c3dd97b67ecadba2dc132cd8e328
Nicolas Azzopardi <nicolas.azzopardi@cnrs.fr>
2022-05-18 17:36:43 +0200

    CENS and LIMIT after Y

--- R/addCENS.LLOQ.R
+++ R/addCENS.LLOQ.R
@@ -13,17 +13,13 @@ addCENS.LLOQ <- function(.data, LLOQ = .1, rm = TRUE) {
   Y <- CENS <- NULL
   if (rm == TRUE) {
     TAB %>%
-      mutate(
-        Y = ifelse(is.na(Y), NA, ifelse(Y <= LLOQ, LLOQ, Y)),
-        CENS = ifelse(is.na(Y), NA, ifelse(Y == LLOQ, 1, 0)),
-        LIMIT = ifelse(CENS == 1, 0, NA)
-      )
+      mutate(Y = ifelse(is.na(Y), NA, ifelse(Y <= LLOQ, LLOQ, Y))) |>
+      mutate(CENS = ifelse(is.na(Y), NA, ifelse(Y == LLOQ, 1, 0)), .after = Y) |>
+      mutate(LIMIT = ifelse(CENS == 1, 0, NA), .after = CENS)
   } else {
     TAB %>%
-      mutate(
-        Y = ifelse(is.na(Y), NA, ifelse(Y <= LLOQ, LLOQ, Y)),
-        CENS = ifelse(is.na(Y), NA, ifelse(Y == LLOQ, 1, CENS)),
-        LIMIT = ifelse(CENS == 1, 0, NA)
-      )
+      mutate(Y = ifelse(is.na(Y), NA, ifelse(Y <= LLOQ, LLOQ, Y))) |>
+      mutate(CENS = ifelse(is.na(Y), NA, ifelse(Y == LLOQ, 1, CENS)), .after = Y) |>
+      mutate(LIMIT = ifelse(CENS == 1, 0, NA), .after = CENS)
   }
 }
